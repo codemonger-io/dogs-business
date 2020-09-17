4f22b0322d64ecf494e6a01c3a329845f422ba33
Kikuo Emoto <kikuomax@gmail.com>
2020-09-18 00:44:15 +0900

    feat: add menu to navbar
    - `navigation-bar.vue` now has a menu.
      Behavior of the burger button is implemented.
      Behavior of the menu is NOT yet implemented.
      `navigation-bar.vue` calls `makeRandomId` (see below) to generate a
      unique ID for the menu element.
    - A new file `utils/random-id.js` defines a new function `makeRandomId`
      that generates a random string to make a unique ID.
      `test/setup-global.js` prepares a fake `window.crypto` so that
      `makeRandomId` can use the `getRandomValues` API during testing.
      DO NOT USE this implementation of `getRandomValues` for production
      because it is not cryptographically safe at all.

--- src/components/navigation-bar.vue
+++ src/components/navigation-bar.vue
@@ -5,10 +5,7 @@
     aria-label="main navigation"
   >
     <div class="navbar-brand">
-      <a
-        class="navbar-item"
-        href="#"
-      >
+      <a class="navbar-item">
         <img src="@assets/images/dogs-business.svg" />
       </a>
       <a
@@ -22,18 +19,37 @@
       <a
         role="button"
         class="navbar-burger"
+        :class="burgerClass"
         aria-label="menu"
         aria-expanded="false"
+        :data-target="navbarMenuId"
+        @click="onBurgerClicked"
       >
         <span aria-hidden="true" />
         <span aria-hidden="true" />
         <span aria-hidden="true" />
       </a>
     </div>
+    <div
+      :id="navbarMenuId"
+      class="navbar-menu"
+      :class="navbarMenuClass"
+    >
+      <div class="navbar-start">
+        <a
+          class="navbar-item"
+          @click.prevent="onEditDogProfileClicked"
+        >
+          Edit dog profile
+        </a>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
+import { makeRandomId } from '@utils/random-id'
+
 /**
  * Vue component that renders a navigation bar.
  *
@@ -42,6 +58,44 @@
  * @memberof module:components
  */
 export default {
-  name: 'NavigationBar'
+  name: 'NavigationBar',
+  data () {
+    return {
+      navbarMenuId: 'navbar-menu',
+      isMenuActive: false
+    }
+  },
+  computed: {
+    burgerClass () {
+      return {
+        'is-active': this.isMenuActive
+      }
+    },
+    navbarMenuClass () {
+      return {
+        'is-active': this.isMenuActive
+      }
+    }
+  },
+  created () {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('NavigationBar', 'created')
+    }
+    // makes a unique id for this navbar
+    this.navbarMenuId = `navbar-menu-${makeRandomId()}`
+  },
+  methods: {
+    onBurgerClicked () {
+      if (process.env.NODE_ENV !== 'production') {
+        console.log('NavigationBar', 'burger clicked')
+      }
+      this.isMenuActive = !this.isMenuActive
+    },
+    onEditDogProfileClicked () {
+      if (process.env.NODE_ENV !== 'production') {
+        console.log('NavigationBar', 'edit dog profile clicked')
+      }
+    }
+  }
 }
 </script>
--- test/setup-global.js
+++ test/setup-global.js
@@ -1,2 +1,16 @@
 // vue-test-utils needs jsdom
 require('jsdom-global')()
+
+// defines window.crypto for testing.
+// only getRandomValues is defined so far.
+window.crypto = {
+  // this implementation is not cryptographically safe at all.
+  // NEVER USE for production.
+  getRandomValues (buf) {
+    // wrapping `buf` with `Uint8Array` simplifies the problem.
+    const buf8 = new Uint8Array(buf.buffer)
+    for (let i = 0; i < buf8.length; ++i) {
+      buf8[i] = Math.floor(256 * Math.random())
+    }
+  }
+}
