67ff4f0f6045ed383625623beaecaa247a14d7c9
Kikuo Emoto <kemoto@codemonger.io>
2025-06-29 13:32:37 +0900

    feat(cdk): deploy PassquitoCore
    Deploys `PassquitoCore` to build an authentication infrastructure.

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,8 @@
 import { CfnOutput, Stack, type StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+import { PassquitoCore } from '@codemonger-io/passquito-cdk-construct';
+
 import type { DeploymentStage } from './deployment-stage';
 import { Distribution } from './distribution';
 
@@ -15,6 +17,13 @@ export class CdkStack extends Stack {
 
     const { deploymentStage } = props;
 
+    const passquito = new PassquitoCore(this, 'Passquito', {
+      ssmParametersProps: {
+        group: 'dogs-business',
+        config: deploymentStage,
+      },
+      allowOrigins: ['http://localhost:5173'],
+    });
     const distribution = new Distribution(this, 'Distribution', {
       deploymentStage,
     });
