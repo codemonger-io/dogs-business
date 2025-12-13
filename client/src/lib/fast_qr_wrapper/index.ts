5c409b53341d331fbd0bc31b060628d6ffd8bf4b
Kikuo Emoto <kemoto@codemonger.io>
2025-12-14 01:00:54 +0900

    feat(client): add wrapper for fast_qr
    `fast_qr` is a package to read QR code, which utilizes WebAssembly to
    achieve high-performance. It internally reads the `fast_qr_bg.wasm` file
    using the `new URL(..., import.meta.url)` syntax. Unfortunately, the
    syntax won't work with Vite if it is used under the `node_modules`
    folder. So I decided to copy the `fast_qr_bg.wasm` file into the
    `lib/fast_qr_wrapper` folder and write a simple function to initialize
    `fast_qr` with the copy of `fast_qr_bg.wasm`. It still uses the
    `new URL(..., import.meta.url)` syntax, though it works fine with Vite.
    The `postinstall` script copies the `fast_qr_bg.wasm` file from
    `fast_qr` into `lib/fast_qr_wrapper`.
    
    A copy of `fast_qr_bg.wasm` should be gitignored.

---
+++ client/src/lib/fast_qr_wrapper/index.ts
@@ -0,0 +1,36 @@
+/**
+ * Wrapper for the `fast_qr` package.
+ *
+ * @remarks
+ *
+ * `fast_qr` internally loads a WebAssembly module file with the
+ * `new URL(..., import.meta.url)` syntax.
+ * This syntax is specially treated by Vite but won't work for files under the
+ * `node_modules` folder.
+ *
+ * To work around this problem, I decided to copy the wasm file,
+ * `fast_qr_bg.wasm`, into this folder and load it from here.
+ * The wasm file is supposed to be copied by the `postinstall` script in
+ * `package.json`.
+ *
+ * @packageDocumentation
+ *
+ * @beta
+ */
+
+import init from 'fast_qr'
+export { SvgOptions, qr_svg as qrSvg } from 'fast_qr'
+
+/**
+ * Initializes `fast_qr`.
+ *
+ * @remarks
+ *
+ * Explicitly specifies the path to the wasm file so that `fast_qr` does not
+ * need to resolve the wasm file by itself.
+ *
+ * @beta
+ */
+export const initFastQr = async () => {
+  return init(new URL('./fast_qr_bg.wasm', import.meta.url))
+}
