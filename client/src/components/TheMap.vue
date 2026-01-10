d79d19dc5f3d3907062f38ebd1784e3e6c27a4af
Kikuo Emoto <kemoto@codemonger.io>
2026-01-10 13:05:16 +0900

    chore(client): explicitly import RouterLink in TheMap

--- client/src/components/TheMap.vue
+++ client/src/components/TheMap.vue
@@ -14,6 +14,7 @@ import {
   watchEffect
 } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { RouterLink } from 'vue-router'
 
 import type { BusinessType } from '../lib/business-record-database'
 import { convertBusinessRecordsToGeoJSON } from '../lib/business-record-database'
