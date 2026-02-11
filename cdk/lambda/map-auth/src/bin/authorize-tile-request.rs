b2c8a959c513b5a86bb2479035d6a1766107d1b0
Kikuo Emoto <kemoto@codemonger.io>
2026-02-11 18:54:53 +0900

    feat(cdk): verify map tile access token
    The `map-auth/authorize-tile-request` app actually verifies map tile
    access tokens.

--- cdk/lambda/map-auth/src/bin/authorize-tile-request.rs
+++ cdk/lambda/map-auth/src/bin/authorize-tile-request.rs
@@ -2,6 +2,10 @@
 //!
 //! ## Environment variables
 //!
+//! You have to configure the following environment variable:
+//! - `TILE_ACCESS_TOKEN_SECRET_PARAMETER_PATH`: Path to the secret key to verify the tile
+//!   access token, that is stored in AWS Systems Manager (SSM) Parameter Store.
+//!
 //! ## Input
 //!
 //! See [`ApiGatewayCustomAuthorizerRequestTypeRequest`](https://docs.rs/aws_lambda_events/latest/aws_lambda_events/event/apigw/struct.ApiGatewayCustomAuthorizerRequestTypeRequest.html).
@@ -18,17 +22,47 @@ use aws_lambda_events::{
     http::HeaderName,
     iam::{IamPolicyEffect, IamPolicyStatement},
 };
+use base64::{engine::general_purpose::STANDARD as base64_engine, Engine as _};
+use hmac::{Hmac, Mac as _};
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
+use sha2::Sha256;
+use std::env;
 use std::sync::Arc;
+use std::time::SystemTime;
+
+use map_auth::{
+    ByteArrayExt as _,
+    TileAccessTokenBytes,
+    TileAccessTokenBytesExt as _,
+};
 
 /// Shared state that lives while the Lambda instance is alive.
 struct SharedState {
+    /// Secret key to verify the tile access token.
+    secret_key: String,
 }
 
 impl SharedState {
     /// Creates a new `SharedState`.
     async fn new() -> Result<Self, Error> {
-        Ok(Self {})
+        // lodas the secret key from SSM Parameter Store
+        let secret_key_path = env::var("TILE_ACCESS_TOKEN_SECRET_PARAMETER_PATH")
+            .map_err(|_| "TILE_ACCESS_TOKEN_SECRET_PARAMETER_PATH env is not set")?;
+        let config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
+        let ssm_client = aws_sdk_ssm::Client::new(&config);
+        let secret_key = ssm_client
+            .get_parameter()
+            .name(secret_key_path)
+            .with_decryption(true)
+            .send()
+            .await?
+            .parameter
+            .and_then(|p| p.value)
+            .ok_or_else(|| "no secret for tile access tokens stored in the Parameter Store")?;
+
+        Ok(Self {
+            secret_key,
+        })
     }
 }
 
@@ -60,12 +94,57 @@ async fn function_handler(
     if token.is_none() {
         tracing::warn!("invalid authorization header. must start with 'Bearer '");
     }
-    // TODO: validate token
-    tracing::info!("extracted token: {:?}", token);
-    let is_valid = token.is_some();
-    if !is_valid {
-        tracing::warn!("invalid authorization token");
-    }
+
+    // validates the token
+    tracing::info!("validating token: {:?}", token);
+    let is_valid = token
+        .ok_or_else(|| ())
+        // decodes the Base64-encoded token → token_bytes
+        .and_then(|token| {
+            if !TileAccessTokenBytes::validate_base64_encoded_length(token.len()) {
+                tracing::warn!("invalid Base64-encoded token length: {}", token.len());
+                return Err(());
+            }
+            let mut token_bytes = TileAccessTokenBytes::zeros();
+            base64_engine
+                .decode_slice(token, &mut token_bytes)
+                .map_err(|e| {
+                    tracing::warn!("invalid Base64-encoded token: {e}");
+                    ()
+                })
+                .and_then(|len| {
+                    if len != token_bytes.len() {
+                        tracing::warn!("invalid decoded token length: {len}");
+                        return Err(());
+                    }
+                    Ok(token_bytes)
+                })
+        })
+        // validates the token signature
+        .and_then(|token_bytes| {
+            let (expires_at, signature) = token_bytes.get_parts();
+            let mut mac = Hmac::<Sha256>::new_from_slice(shared_state.secret_key.as_bytes())
+                .map_err(|e| {
+                    tracing::error!("wrong secret key configuration: {e}");
+                    ()
+                })?;
+            mac.update(&expires_at);
+            mac.verify_slice(&signature)
+                .map_err(|e| {
+                    tracing::error!("invalid token signature: {e}");
+                    ()
+                })?;
+            Ok(u64::from_be_bytes(expires_at))
+        })
+        // validates the expiration time
+        .map(|expires_at| {
+            let now = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("UNIX time should be valid")
+                .as_secs();
+            now < expires_at
+        })
+        .is_ok_and(|validity| validity);
 
     // configures the policy
     let policy_document = &mut response.policy_document;
