853501d28bbd9508ae6222ef190c43dabd55f110
Kikuo Emoto <kemoto@codemonger.io>
2026-03-04 19:14:35 +0900

    feat(client): add location marker icon
    Introduces a new CSS class `.is-stroke-icon` for `.icon`, which has
    the same properties as the `.is-lucide-icon` but for icons not taken
    from `lucide-icons`.

---
+++ client/src/components/icons/IconLocationMarker.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import iconSvg from '../../assets/icons/location-marker.svg?raw'
+</script>
+
+<template>
+  <span class="icon is-stroke-icon" v-html="iconSvg"></span>
+</template>
