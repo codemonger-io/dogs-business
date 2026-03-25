b61d2845fab1cfcc4f790cabec73853f227f355e
Kikuo Emoto <kemoto@codemonger.io>
2026-03-25 23:34:38 +0900

    chore(client): add heart-shield icon
    To represent a "guardian".

---
+++ client/src/components/icons/IconHeartShield.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import iconSvg from '../../assets/icons/heart-shield.svg?raw'
+</script>
+
+<template>
+  <span class="icon is-stroke-icon" v-html="iconSvg"></span>
+</template>
